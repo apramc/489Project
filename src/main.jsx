5bc63514fc8aa4adcbb5a8f446494ef6de598296
kytxii <tilleykyle12@gmail.com>
2025-03-02 20:22:10 -0800

    Updated homepage handling in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
-import App from './Homepage.jsx'
+import Homepage from './Homepage.jsx'
 import LoginSignupPage from './LoginSignupPage.jsx';
 import SignupPage from './SignupPage.jsx';
 import AdminDashboard from './AdminDashboard.jsx';
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/home" element={<Homepage />} />
         <Route path="/login" element={<LoginSignupPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/admin" element={<AdminDashboard />} />
